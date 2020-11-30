45f81dc3c039dd4e003af8a6b4b89b9168d93b53
applislaurent <l.perroy@wanadoo.fr>
2020-11-30 14:20:21 +0100

    modif DemmarageordiApplication

--- src/main/java/demarrageordi/DemarrageordiApplication.java
+++ src/main/java/demarrageordi/DemarrageordiApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 // Pour une utulisation sans BDD:
@@ -13,6 +16,16 @@ public class DemarrageordiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemarrageordiApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("telecharger.batch").allowedOrigins("*");
+			}
+		};
 	}
 
 }
