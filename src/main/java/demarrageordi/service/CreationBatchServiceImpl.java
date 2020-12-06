fbeeb8b8e9223f2f5280f090988cbed4057e6b77
applislaurent <l.perroy@wanadoo.fr>
2020-12-06 08:38:52 +0100

    changement user.dir

--- src/main/java/demarrageordi/service/CreationBatchServiceImpl.java
+++ src/main/java/demarrageordi/service/CreationBatchServiceImpl.java
@@ -191,9 +191,7 @@ public class CreationBatchServiceImpl implements CreationBatchService {
 	 */
 	private String chercherChemin(String repertoire, String nomlogiciel) {
 
-		if (!repertoire.startsWith("cd /mnt/c")) {
-			repertoire = "cd /mnt/c" + repertoire;
-		}
+		repertoire.replaceAll("C:\\", "cd /mnt/c");
 		File[] files = new File(repertoire).listFiles();
 
 		if (files != null) {
